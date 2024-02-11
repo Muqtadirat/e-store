63cfed9396d1811fcab99ee24b60c95f0c669f49
Muqtadirat <muqtadir.my@gmail.com>
2024-02-11 19:50:18 +0100

    feat: created .env to store api

--- client/src/lib/api/categories.js
+++ client/src/lib/api/categories.js
@@ -1,6 +1,9 @@
+const apiUrl = import.meta.env.VITE_POSTMAN_API_URL
+
 export default async function FetchCategories() {
   try {
-    const res = await fetch("http://localhost:5000/v1/categories");
+    // const res = await fetch("http://localhost:5000/v1/categories"); //Backend url
+    const res = await fetch(`${apiUrl}categories`)
     const data = res.json();
    
     return data;
