de4ef86a4296306005b6a6e096e2cb27b9992a76
Muqtadirat <muqtadir.my@gmail.com>
2024-02-13 09:57:49 +0100

    feat: implemented sales data fetching

--- client/src/lib/api/index.js
+++ client/src/lib/api/index.js
@@ -1,2 +1,3 @@
 export { default as fetchCategories } from "./categories";
-export {default as fetchDeals} from "./deals"
+export { default as fetchDeals } from "./deals"
+export {default as fetchSales} from "./sales"
